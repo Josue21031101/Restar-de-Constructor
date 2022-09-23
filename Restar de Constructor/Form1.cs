34b8932b2c4c71280e89401741c5dfc76d5f77f6
josue <josue@LAPTOP-I3VRBKPN>
2022-09-23 09:29:19 -0600

    Agregar archivos de proyecto.

---
+++ Restar de Constructor/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Restar_de_Constructor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Resta s = new Resta(double.Parse(n1.Text)
+               , double.Parse(n2.Text));
+
+            n3.Text = s.Restar().ToString();
+        }
+    }
+}
---
+++ Restar de Constructor/Resta.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restar_de_Constructor
+{
+    internal class Resta
+    {
+        double v1, v2;
+
+        public Resta()
+        {
+        }
+
+        public Resta (double v1, double v2)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+        }
+
+        public double Restar()
+        {
+            return v1 - v2;
+        }
+    }
+}
